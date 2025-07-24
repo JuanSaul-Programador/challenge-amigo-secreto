c3eb111c5369142c862ddffd80e93ad81ae182e6
JuanSaul-Programador <jpereyraacedo87@gmail.com>
2025-07-24 16:56:20 -0500

    Realizamos la funcion mostrarListaAmigos

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ let amigos = []
 
 function agregarAmigo(){
 
-    let nombre  = document.getElementById('amigo').value;
+        let nombre  = document.getElementById('amigo').value;
 
-    if(nombre === ''){
-        alert('Por favor inserte un nombre');
-        return;
-    }
-
-     amigos.push(nombre);
-
-     document.getElementById('amigo').value ='';
+        if(nombre === ''){
+            alert('Por favor inserte un nombre');
+            return;
+        }
+        amigos.push(nombre);
+       mostrarListaAmigos()
+        document.getElementById('amigo').value ='';
 }
+
+function mostrarListaAmigos(){
+       //Obtenemos lista del elemento
+       let lista = document.getElementById('listaAmigos')
+       //Limpiamos Lista
+       lista.innerHTML = "";
+       //Iteramos sobre el arreglo
+       for(let i = 0; i < amigos.length; i++){
+           //Agregamos elementos
+           lista.innerHTML += "<li>" + amigos[i] + "</li>"
+       }
+}
+
