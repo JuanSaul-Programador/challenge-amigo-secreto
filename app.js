d0ad3dfa0aa9efdc76c34d1e9cc0b9e9558c8d4b
JuanSaul-Programador <jpereyraacedo87@gmail.com>
2025-07-24 17:20:53 -0500

    Challenge Terminado

--- app.js
+++ app.js
@@ -2,15 +2,21 @@
 let amigos = []
 
 function agregarAmigo(){
-
+        //Capturamos el valor del campo de entrada
         let nombre  = document.getElementById('amigo').value;
 
+        //Validamos que el campo no este vacio
         if(nombre === ''){
             alert('Por favor inserte un nombre');
             return;
         }
+        //Actualizamos el array de amigos
         amigos.push(nombre);
-       mostrarListaAmigos()
+
+        //Mostramos lista de amigos
+        mostrarListaAmigos()
+
+       //Limpiamos campo de entrada
         document.getElementById('amigo').value ='';
 }
 
@@ -21,8 +27,25 @@ function mostrarListaAmigos(){
        lista.innerHTML = "";
        //Iteramos sobre el arreglo
        for(let i = 0; i < amigos.length; i++){
-           //Agregamos elementos
            lista.innerHTML += "<li>" + amigos[i] + "</li>"
        }
 }
 
+
+function sortearAmigo(){
+    //Validamos amigos disponibles
+    if(amigos.length === 0){
+        alert('No hay amigos disponibles para sortear.');
+        return;
+    }
+    //Generamos un indice aleatorio
+    let indiceAletorio = Math.floor(Math.random()* amigos.length);
+    //Obtenemos el nombre aleatorio
+    let amigoSorteado = amigos[indiceAletorio];
+
+    //Limpiamos lista de amigos
+    document.getElementById('listaAmigos').innerHTML = '';
+ 
+    //Mostramos el resultado
+    document.getElementById('resultado').innerHTML = `El amigo sorteado es : ${amigoSorteado}`;
+}
